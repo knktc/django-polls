ffe5d032198834beb2de0a53762c459db3969353
knktc <me@knktc.com>
2018-07-14 16:44:16 +0800

    add base model

---
+++ polls/models.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@author:knktc
+@contact:me@knktc.com
+@create:2018-07-14 16:38
+"""
+
+from django.db import models
+
+__author__ = 'knktc'
+__version__ = '0.1'
+
+
+class BaseModel(models.Model):
+    enable = models.BooleanField(default=True)
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True)
+
+
